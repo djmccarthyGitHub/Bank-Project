384a3ab39fa5fe4ba99fbfcd69a5690f9ef0ab0a
David McCarthy <McCarthyJDavid@outlook.com>
2022-12-06 19:56:14 -0800

    added create account layout and functions

---
+++ website/scripts/createAccount.js
@@ -0,0 +1,9 @@
+function attemptCreateAccount() {
+    // insert attempt create account code here
+    location.href = "../pages/dashboard.html";
+}
+
+function cancelCreateAccount() {
+    // insert cancel create account code here
+    location.href = "../pages/login.html";
+}
